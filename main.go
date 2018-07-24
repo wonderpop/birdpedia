d72ef414fa2b043ef920bde776999ac553a6b881
Charles Baldwin <baldwinc@ecu.edu>
2018-07-24 16:20:17 -0400

    First part of tutorial done and functional

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	staticFileDirectory := http.Dir("./assets/")
+
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+	return r
+}
+
+func main() {
+	r := newRouter()
+
+	http.ListenAndServe(":8080", r)
+}
+
+// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
+// as the arguments.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
